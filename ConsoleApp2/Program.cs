2bd0e0e5e9f942eb4d9a02ce1a68e60cc28f81ee
unknown <cogdotnet902@iiht.tech>
2022-06-29 15:36:03 +0530

    cousumer

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory
+            {
+                Uri = new System.Uri("amqp://guest:guest@localhost:5672")
+            };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            QueueConsumer.consume(channel);
+        }
+    }
+}
---
+++ ConsoleApp2/QueueConsumer.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+   public static  class QueueConsumer
+    {
+        public static void consume(IModel channel)
+        {
+            channel.QueueDeclare("batch22.queuel",
+             durable:true,
+               exclusive: false,
+               autoDelete: false,
+               arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+             {
+                 var body = e.Body.ToArray();
+                 var message = Encoding.UTF8.GetString(body);
+                 Console.WriteLine(message);
+             };
+            channel.BasicConsume("batch22.queuel", true, consumer);
+            Console.WriteLine("Consumer Started");
+            Console.ReadLine();
+        }
+    }
+}
